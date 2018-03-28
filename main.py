f1d842c28fd56c3f4d202db67fd93fc996553112
Lucas Magno <lucasmagnorc@gmail.com>
2018-03-28 19:52:04 -0300

    created class for token object

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
+from tokens import token
 
-with open("pasC1.txt") as file:
-	program = "program"
-	while True:
-		c = file.read(1)
-		for item in c:
-			if c.isalpha():
-				print (c)
-		if not c:
-			print("End of file")
-			break
-		
+def openFile(nameFile):
+	file = open(nameFile, 'r')
+	textArq = file.readlines()
+
+openFile("pasC1.txt")
+
+tokenTeste = token("EOF", "EOF", 0, 21)
+
+print(str(tokenTeste))
+
---
+++ tokens.py
@@ -0,0 +1,33 @@
+class token(object):
+    tag = ""
+    value = ""
+    row = 0
+    column = 0
+    def __init__(self, tag, value, row, column):
+        self.tag = tag
+        self.value = value
+        self.row = row
+        self.column = column
+        
+    def getTag(self):
+        return self.tag
+    def setTag(self, tag):
+        self.tag = tag
+    
+    def getValue(self):
+        return self.value
+    def setValue(self, value):
+        self.value = value
+    
+    def getRow(self):
+        return self.row
+    def setRow(self, row):
+        self.row = row
+    
+    def getColumn(self):
+        return self.column
+    def setColumn(self, column):
+        self.column = column
+
+    def __str__(self):
+        return "<"+str(self.tag)+", \""+str(self.value)+"\">" 
